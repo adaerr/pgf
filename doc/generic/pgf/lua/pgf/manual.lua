74240ceb0a85b3e745236bc7a54186789cbf7e0b
tantau <tantau>
2013-04-04 20:43:29 +0000

    *** empty log message ***

---
+++ doc/generic/pgf/lua/pgf/manual.lua
@@ -0,0 +1,18 @@
+-- Copyright 2013 by Till Tantau
+--
+-- This file may be distributed an/or modified
+--
+-- 1. under the LaTeX Project Public License and/or
+-- 2. under the GNU Public License
+--
+-- See the file doc/generic/pgf/licenses/LICENSE for more information
+
+-- @release $Header: /home/nmelzer/projects/TeX/pgf/doc/generic/pgf/lua/pgf/Attic/manual.lua,v 1.1 2013/04/04 20:43:29 tantau Exp $
+
+
+
+local manual = {}
+
+require 'pgf'.manual = manual
+
+return manual
